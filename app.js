35848ab94bbae9239a73117fe84247a50487d207
AbolfazlAbz <abolfazl84abz@gmail.com>
2023-05-06 21:18:02 +0430

    Logging of user names was done

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ alert('Hello World!')
 // function ...
 console.log('hi');
 console.log('salam');
+
+let names = ['ali' , 'amir' , 'amin' , 'ali' , 'ahmad']
+
+for(const item of names){
+    console.log(item);
+}
